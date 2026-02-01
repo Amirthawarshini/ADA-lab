abd2a71168eff218872ae8c3d77ef789ab5c22ff
Amirthawarshini <amirthawarshini08@gmail.com>
2026-02-01 21:33:23 +0530

    Added file

---
+++ Non-Recursive_Factorial.py
@@ -0,0 +1,16 @@
+num = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, num + 1):
+    fact = fact * i
+
+print("Factorial =", fact)
+
+
+# Step 1: Start
+# Step 2: Read a number n
+# Step 3: Set factorial = 1
+# Step 4: Repeat from i = 1 to n
+#   Multiply factorial by i
+# Step 5: Print factorial value
+# Step 6: Stop
